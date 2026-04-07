3f963c91f6207d2c24d60cfd8e38c557ffb01634
Анастасия Стригина <Anastasiya.Strigina@cyberprotect.ru>
2026-04-07 18:35:45 +0300

    service2

---
+++ go.mod
@@ -0,0 +1,5 @@
+module dictionary-service
+
+go 1.25.6
+
+require github.com/lib/pq v1.12.0
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"dictionary-service/config"
+	"dictionary-service/server"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+type Config struct {
+	Port     int    `json:"port"`
+	LogLevel string `json:"log_level"`
+	Database struct {
+		Host     string `json:"host"`
+		Port     int    `json:"port"`
+		User     string `json:"user"`
+		Password string `json:"password"`
+		DBName   string `json:"dbname"`
+		SSLMode  string `json:"ssl_mode"`
+	} `json:"database"`
+}
+
+func main() {
+	// Шаг 1: загрузка конфигурации
+	cfg, err := config.LoadConfig("config/service2.json")
+	if err != nil {
+		log.Fatalf("Ошибка загрузки конфигурации: %v", err)
+	}
+
+	s := server.NewServer(cfg)
+	s.Start(cfg)
+
+}
